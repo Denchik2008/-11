7fc35011180f307eb8cb573e7bc160815d9d2382
Denchik2008 <d.chenchenko@student.univers.su>
2026-02-27 14:41:40 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def q():
+    return 'Миссия колонизации Марса'
+@app.route('/index')
+def index():
+    return 'И на марсе будут яблони цвести'
+@app.route('/promotion')
+def promotion():
+    return '''Человечество вырастает из детства.\n
+
+Человечеству мала одна планета.\n
+
+Мы сделаем обитаемыми безжизненные пока планеты.\n
+
+И начнем с Марса!\n
+
+Присоединяйся!\n'''
+
+@app.route('/image_mars')
+def img_mars():
+    # return 'dsgfsdgfsdgs'
+    return render_template('1.html')
+
+if __name__ == '__main__':
+    app.run(port=5000, host='127.0.0.1')
